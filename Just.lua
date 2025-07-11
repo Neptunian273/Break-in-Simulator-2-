90e631a0c925efbda83cc9f07801cd2ad4ee38e3
Neptunian273 <ehd0628ehd@gmail.com>
2025-07-12 02:42:49 +0900

    Create Just.lua
    Just

---
+++ Just.lua
@@ -0,0 +1,335 @@
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
+local Window = Rayfield:CreateWindow({
+	Name = "The Endless Deads Battlefield",
+	LoadingTitle = "Loading...",
+	LoadingSubtitle = "Please wait",
+	ConfigurationSaving = { Enabled = false },
+	Theme = "Dark"
+})
+
+local Tab = Window:CreateTab("Planks")
+
+Tab:CreateButton({
+	Name = "Block all",
+	Callback = function()
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, 16.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060756, 0.0243060756, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, -8.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060794, 0.0243060794, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, -52.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060868, 0.0243060868, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -52.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060905, 0.0243060905, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -37.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060943, 0.0243060943, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -8.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.024306098, 0.024306098, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, 13.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243061017, 0.0243061017, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(80.8831177, 10.5110645, -69, -0.0655616894, 3.98020939e-06, -0.997848511, -0.997848511, 6.40904858e-08, 0.0655617416, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(104.883118, 26.5110645, -69, -0.0655617565, 3.98020939e-06, -0.997848511, -0.997848511, 6.40902726e-08, 0.0655617565, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(79.8831177, 26.5110645, -69, -0.0655617565, 3.98020939e-06, -0.997848511, -0.997848511, 6.40902726e-08, 0.0655617565, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(100.883118, 10.5110645, -69, -0.0810394585, 3.48099238e-05, -0.996710896, 0.996710837, 1.16767187e-05, -0.0810394585, 8.81732558e-06, -0.99999994, -3.5641704e-05) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(121.5, 10.6419983, 16.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 10.6419983, -15.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, 13.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -8.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -30.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -52.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(105.883118, 10.5110645, 30, 0.136803567, 4.32432862e-05, -0.990598202, 0.990598202, -4.93142288e-05, 0.136803567, -4.29347565e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(78.8831177, 10.5110645, 30, 0.136803567, 4.32433044e-05, -0.990598202, 0.990598202, -4.93140833e-05, 0.136803567, -4.2934611e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(82.8831177, 26.5110645, 30, 0.136803567, 4.32433153e-05, -0.990598202, 0.990598202, -4.9313996e-05, 0.136803567, -4.29345237e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(104.883118, 26.5110645, 30, 0.136803567, 4.32433189e-05, -0.990598202, 0.990598202, -4.93139669e-05, 0.136803567, -4.29344946e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(66.5, 11.6419983, -30.5, -2.17398028e-05, -1, -1.59442425e-05, -0.30480364, 2.18119094e-05, -0.952415228, 0.952415228, -1.58454568e-05, -0.30480364) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 8.64199829, -30.5, -2.17398028e-05, -1, -1.59442425e-05, -0.30480364, 2.18119094e-05, -0.952415228, 0.952415228, -1.58454568e-05, -0.30480364) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+	end,
+})
+
+Tab:CreateButton({
+	Name = "loop Block all",
+	Callback = function()
+while wait(0.5) do
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, 16.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060756, 0.0243060756, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, -8.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060794, 0.0243060794, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 10.6419983, -52.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060868, 0.0243060868, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -52.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060905, 0.0243060905, -1.8071727e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -37.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243060943, 0.0243060943, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, -8.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.024306098, 0.024306098, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 26.6419983, 13.5, 4.46597733e-05, -1, -1.91628933e-05, 0.99970454, 4.51123524e-05, -0.0243061017, 0.0243061017, -1.80717252e-05, 0.99970454) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(80.8831177, 10.5110645, -69, -0.0655616894, 3.98020939e-06, -0.997848511, -0.997848511, 6.40904858e-08, 0.0655617416, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(104.883118, 26.5110645, -69, -0.0655617565, 3.98020939e-06, -0.997848511, -0.997848511, 6.40902726e-08, 0.0655617565, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(79.8831177, 26.5110645, -69, -0.0655617565, 3.98020939e-06, -0.997848511, -0.997848511, 6.40902726e-08, 0.0655617565, 3.24900611e-07, 1, 3.96744417e-06) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(100.883118, 10.5110645, -69, -0.0810394585, 3.48099238e-05, -0.996710896, 0.996710837, 1.16767187e-05, -0.0810394585, 8.81732558e-06, -0.99999994, -3.5641704e-05) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(121.5, 10.6419983, 16.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 10.6419983, -15.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, 13.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -8.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -30.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(121.5, 26.6419983, -52.5, 7.16010691e-05, -1, 5.83529472e-05, 0.2020863, 7.16185896e-05, 0.979367733, -0.979367733, -5.83314431e-05, 0.2020863),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(105.883118, 10.5110645, 30, 0.136803567, 4.32432862e-05, -0.990598202, 0.990598202, -4.93142288e-05, 0.136803567, -4.29347565e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(78.8831177, 10.5110645, 30, 0.136803567, 4.32433044e-05, -0.990598202, 0.990598202, -4.93140833e-05, 0.136803567, -4.2934611e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(82.8831177, 26.5110645, 30, 0.136803567, 4.32433153e-05, -0.990598202, 0.990598202, -4.9313996e-05, 0.136803567, -4.29345237e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(104.883118, 26.5110645, 30, 0.136803567, 4.32433189e-05, -0.990598202, 0.990598202, -4.93139669e-05, 0.136803567, -4.29344946e-05, -1, -4.95830827e-05),
+    [2] = Vector3.new(0, 0, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args)) local args = {
+    [1] = CFrame.new(66.5, 11.6419983, -30.5, -2.17398028e-05, -1, -1.59442425e-05, -0.30480364, 2.18119094e-05, -0.952415228, 0.952415228, -1.58454568e-05, -0.30480364) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+local args = {
+    [1] = CFrame.new(66.5, 8.64199829, -30.5, -2.17398028e-05, -1, -1.59442425e-05, -0.30480364, 2.18119094e-05, -0.952415228, 0.952415228, -1.58454568e-05, -0.30480364) * CFrame.Angles(0, 1.5707963705062866, 0),
+    [2] = Vector3.new(0, 90, 0)
+}
+
+game:GetService("ReplicatedStorage").RemoteEvents.PutBoardOn:FireServer(unpack(args))
+end
+	end,
+})
+
+local Tab = Window:CreateTab("Players", 8585683407)
+
+Tab:CreateButton({
+	Name = "Kill all",
+	Callback = function()
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Weapon = LocalPlayer.Character:FindFirstChild("Pistol")
+
+    if not Weapon or not Weapon:FindFirstChild("DealDamage") then
+        warn("...")
+        continue
+    end
+
+    for _, player in pairs(Players:GetPlayers()) do
+        if player ~= LocalPlayer and player.Character and player.Character:FindFirstChild("Humanoid") and player.Character:FindFirstChild("Humanoid").Health > 0 then
+            local args = {
+                [1] = player.Character,
+                [2] = 15
+            }
+
+            Weapon.DealDamage:FireServer(unpack(args))
+    end
+end
+	end,
+})
+
+Tab:CreateButton({
+	Name = "loop kill all",
+	Callback = function()
+while wait(0.5) do
+    local Players = game:GetService("Players")
+    local LocalPlayer = Players.LocalPlayer
+    local Weapon = LocalPlayer.Character:FindFirstChild("Pistol")
+
+    if not Weapon or not Weapon:FindFirstChild("DealDamage") then
+        warn("무기가 없거나 DealDamage가 없음")
+        continue
+    end
+
+    for _, player in pairs(Players:GetPlayers()) do
+        if player ~= LocalPlayer and player.Character and player.Character:FindFirstChild("Humanoid") and player.Character:FindFirstChild("Humanoid").Health > 0 then
+            local args = {
+                [1] = player.Character,
+                [2] = 15
+            }
+
+            Weapon.DealDamage:FireServer(unpack(args))
+        end
+    end
+end
+	end,
+})
